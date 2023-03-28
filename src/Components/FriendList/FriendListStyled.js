f3c64667dc07d2cd75fdca03d3452633ba5ae1ba
Antonina Pasichnyk <110766517+Tonja1@users.noreply.github.com>
2023-03-28 11:41:32 +0300

    update

--- src/Components/FriendList/FriendListStyled.js
+++ src/Components/FriendList/FriendListStyled.js
@@ -4,17 +4,3 @@ export const List = styled.ul`
   list-style: none;
 `;
 
-// export const Item = styled.li`
-//   display: flex;
-//   margin-bottom: 5px;
-//   background-color: ${props => {
-//     if (props.isOnline) {
-//       return 'green';
-//     }
-//     return 'red';
-//   }};
-// `;
-
-// export const UserName = styled.p`
-//   margin-left: 15px;
-// `;
